9946916fc3acbdf1c6d1e2980a0964cb688aa0e9
stephenice <stephencode@hotmil.com>
2022-06-26 15:33:29 +0100

    fix nav

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
-const menuItems = document.querySelectorAll(".nav-menu");
+const menuItems = document.querySelector(".nav-menu");
 
-console.log("Hello");
-
-  hamburgerIcon.addEventListener("click", () => {
+hamburgerIcon.addEventListener("click", (e) => {
   hamburgerIcon.classList.toggle("active");
   menuItems.classList.toggle("active");
-
+  console.log("click");
 });
 
-document.querySelector(".nav-links").forEach (n => n.addEventListener("click", () => {
-  hamburgerIcon.classList.remove("active");
-  menuItems.classList.remove("active");
-
-}));
+// document.querySelector(".nav-links").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     hamburgerIcon.classList.remove("active");
+//     menuItems.classList.remove("active");
+//   })
+// );
 
 // INPUT ANIMATION

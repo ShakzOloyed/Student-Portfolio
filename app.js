b8d0dfd974ce17bb35ab2bd6bdb0234166b1cf28
ShakzOloyed <bim0514@hotmail.co.uk>
2022-09-30 18:35:32 +0100

    add updated section

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-const menuItems = document.querySelector(".nav-menu");
+// const hamburgerIcon = document.querySelector(".hamburger-icon");
+// const menuItems = document.querySelector(".nav-menu");
 
-console.log("Hello");
+// console.log("Hello");
 
-  hamburgerIcon.addEventListener("click", (e) => {
-  hamburgerIcon.classList.toggle("active");
-  menuItems.classList.toggle("active");
-  console.log("click")
+//   hamburgerIcon.addEventListener("click", (e) => {
+//   hamburgerIcon.classList.toggle("active");
+//   menuItems.classList.toggle("active");
+//   hamburgerIcon.toggle('.block');
 
-});
+//   console.log("click")
+  
+
+// });
 
 // document.querySelector(".nav-links").forEach (n => n.addEventListener("click", () => {
 //   hamburgerIcon.classList.remove("active");
@@ -17,3 +20,32 @@ console.log("Hello");
 // }));
 
 // INPUT ANIMATION
+'use strict';
+const ham = document.querySelector('.header .nav-menu .menu-list .hamburger-icon');
+const mobile_menu = document.querySelector('.header .nav-menu .menu-list ul');
+const menuItems = document.querySelectorAll('.header .nav-menu .menu-list ul li a');
+const header = document.querySelector('.header .container .nav-menu');
+
+ham.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+// console.log(ham);
+
+document.addEventListener('scroll', () => {
+  let scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = '#29323c';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+})
+
+menuItems.forEach((items) => {
+    items.addEventListener('click', () => {
+      ham.classList.toggle('active');
+      mobile_menu.classList.toggle('active');
+
+    });
+}); 
+
